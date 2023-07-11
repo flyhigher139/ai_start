a72e4c721c0d209580b737f5407ca2331b068c7a
gevin <flyhigher139@gmail.com>
2023-07-11 09:49:24 +0800

    fix bugs

--- code/openai_102.py
+++ code/openai_102.py
@@ -11,12 +11,12 @@ openai.api_key = os.environ["AZURE_OPENAI_KEY"]
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 # 获取"好评"和"差评"的embedding
-positive_preview = get_embedding("好评")
-negative_preview = get_embedding("差评")
+positive_preview = get_embedding("好评", EMBEDDING_MODEL)
+negative_preview = get_embedding("差评", EMBEDDING_MODEL)
 
 positive_example = get_embedding(
-    "买的银色版真的很好看，一天就到了，晚上就开始拿起来完系统很丝滑流畅，做工扎实，手感细腻，很精致哦苹果一如既往的好品质")
-negative_example = get_embedding("降价厉害，保价不合理，不推荐")
+    "买的银色版真的很好看，一天就到了，晚上就开始拿起来完系统很丝滑流畅，做工扎实，手感细腻，很精致哦苹果一如既往的好品质", EMBEDDING_MODEL)
+negative_example = get_embedding("降价厉害，保价不合理，不推荐", EMBEDDING_MODEL)
 
 
 def get_score(sample_embedding):
